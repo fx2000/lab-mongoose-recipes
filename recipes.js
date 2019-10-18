5f6205815db2ac587871ba03d948d38ca88902f5
Daniel Duque <duque@outlook.com>
2019-10-18 13:41:20 +0200

    Finished schema creation

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./model/Recipe');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +9,18 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+Recipe.create({
+  title: 'Delicious Copy Pasta',
+  level: 'Easy Peasy',
+  ingredients: ['salt', 'pepper', 'unoriginal content'],
+  cuisine: 'French',
+  dishType: 'Snack',
+  creator: 'Daniel Duque'
+})
+.then(recipe => {
+  console.log(`Recipe saved: ${recipe}`)
+})
+.catch(err => {
+  console.log(`An error happened: ${err}`)
+});
