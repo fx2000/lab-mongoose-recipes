6fc9591dad4328259cdf93ad6145ef206a550349
Daniel Duque <duque@outlook.com>
2019-10-18 14:04:17 +0200

    Iteration 5 completed

--- recipes.js
+++ recipes.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
 const data = require('./data.js');
 const Recipe = require('./model/Recipe');
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err);
   });
 
+  /* Iteration 2 - Create a recipe
 Recipe.create({
   title: 'Delicious Copy Pasta',
   level: 'Easy Peasy',
@@ -19,8 +20,39 @@ Recipe.create({
   creator: 'Daniel Duque'
 })
 .then(recipe => {
-  console.log(`Recipe saved: ${recipe}`)
+  console.log(`Recipe saved: ${recipe}`);
 })
 .catch(err => {
-  console.log(`An error happened: ${err}`)
+  console.log(`An error happened: ${err}`);
 });
+*/
+
+
+/* Iteration 3 - Insert Many recipes
+Recipe.insertMany(data)
+  .then(recipe => {
+    console.log(`Recipe saved: ${recipe}`);
+  })
+  .catch(err => {
+    console.log(`An error happened: ${err}`);
+  });
+   */
+
+/* Iteration 4 - Update recipe
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then(recipe => {
+      console.log(`Recipe updated: "${recipe}" now has duration: ${recipe}`);
+  })
+  .catch(err => {
+    console.log(`An error happened: ${err}`);
+  });
+  */
+
+/* Iteration 5 - Remove a recipe */
+Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(recipe => {
+    console.log(`Recipe deleted: "${recipe}"`);
+  })
+  .catch(err => {
+    console.log(`An error happened: ${err}`);
+  });
